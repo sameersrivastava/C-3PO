d844a4e7529c76f143aed3a51458de89f110b2b5
sameersrivastava <sameer.srivastava95@gmail.com>
2017-05-09 09:49:33 -0500

    Revert "Trying a simpler callback."
    This reverts commit ade83f9bf05a55927d588f90d16482b3aaedd826.

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function respond() {
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage('Let me get the movies playing now for you','textOnly', nowPlaying);
+    nowPlaying();
     this.res.end();
   } else {
     console.log("don't care");
@@ -77,7 +77,7 @@ function nowPlaying(){
 
 }
 
-function postMessage(botResponse, imageURL, callBack) {
+function postMessage(botResponse, imageURL) {
   imageURL = (typeof imageURL === 'undefined') ? 'textOnly' : imageURL;
 
   var botReq, body;
@@ -122,10 +122,7 @@ function postMessage(botResponse, imageURL, callBack) {
   botReq.on('timeout', function(err) {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
-  botReq.end(function() {
-    JSON.stringify(body);
-    callBack();
-  });
+  botReq.end(JSON.stringify(body));
 
 }
 
