63f03b4f6c9224df6ccd77ea70df895b345e6d79
sameersrivastava <sameer.srivastava95@gmail.com>
2017-05-08 22:28:17 -0500

    Got undefinded, so waiting for whole response to come back.

--- bot.js
+++ bot.js
@@ -52,45 +52,48 @@ function postMessage() {
       var wholeResult = Buffer.concat(chunks);
       console.log(body.toString());
       botResponse = wholeResult.toString();
+
+      //Send data to GroupMe
+      if(typeof botResponse == 'undefined'){
+        botResponse = cool();
+      }
+
+
+      options = {
+        hostname: 'api.groupme.com',
+        path: '/v3/bots/post',
+        method: 'POST'
+      };
+
+      body = {
+        "bot_id" : botID,
+        "text" : botResponse
+      };
+
+      console.log('sending ' + botResponse + ' to ' + botID);
+
+      botReq = HTTPS.request(options, function(res) {
+          if(res.statusCode == 202) {
+            //neat
+          } else {
+            console.log('rejecting bad status code ' + res.statusCode);
+          }
+      });
+
+      botReq.on('error', function(err) {
+        console.log('error posting message '  + JSON.stringify(err));
+      });
+      botReq.on('timeout', function(err) {
+        console.log('timeout posting message '  + JSON.stringify(err));
+      });
+      botReq.end(JSON.stringify(body));
     });
   });
 
   req.write("{}");
   req.end();
+
   
-  if(typeof botResponse == 'undefined'){
-    botResponse = cool();
-  }
-
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
 }
 
 
